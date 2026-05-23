3363152357a34aad8c6503d4517d78ad813692a5
R0me0Star <eip_spb@mail.ru>
2026-05-23 14:00:28 +0300

    dynamic array & string utils

---
+++ pozdnyakov.egor/S3/stringUtils.hpp
@@ -0,0 +1,40 @@
+#ifndef STRING_UTILS_HPP
+#define STRING_UTILS_HPP
+
+#include <string>
+#include "vector.hpp"
+
+namespace pozdnyakov
+{
+  namespace utils
+  {
+    inline Vector< std::string > splitBySpace(const std::string &str)
+    {
+      Vector< std::string > tokens;
+      std::size_t start = 0;
+      std::size_t end = 0;
+
+      while ((start = str.find_first_not_of(" \t\r\n", end)) != std::string::npos) {
+        end = str.find_first_of(" \t\r\n", start);
+        tokens.pushBack(str.substr(start, end - start));
+      }
+
+      return tokens;
+    }
+
+    inline bool isUnsignedNumber(const std::string &str)
+    {
+      if (str.empty()) {
+        return false;
+      }
+      for (char c : str) {
+        if (c < '0' || c > '9') {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
+
+#endif
---
+++ pozdnyakov.egor/S3/vector.hpp
@@ -0,0 +1,181 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <cstddef>
+#include <utility>
+
+namespace pozdnyakov
+{
+  template < class T >
+  class Vector
+  {
+  private:
+    T *data_;
+    std::size_t size_;
+    std::size_t capacity_;
+
+    void reallocate(std::size_t new_capacity)
+    {
+      if (new_capacity == 0) {
+        new_capacity = 1;
+      }
+      T *new_data = new T[new_capacity];
+      for (std::size_t i = 0; i < size_; ++i) {
+        new_data[i] = std::move(data_[i]);
+      }
+      delete[] data_;
+      data_ = new_data;
+      capacity_ = new_capacity;
+    }
+
+  public:
+    using value_type = T;
+    using iterator = T *;
+    using const_iterator = const T *;
+    using reference = T &;
+    using const_reference = const T &;
+
+    Vector():
+      data_(nullptr),
+      size_(0),
+      capacity_(0)
+    {}
+
+    Vector(std::size_t count, const T &value = T()):
+      data_(nullptr),
+      size_(0),
+      capacity_(0)
+    {
+      if (count > 0) {
+        reallocate(count);
+        for (std::size_t i = 0; i < count; ++i) {
+          data_[i] = value;
+        }
+        size_ = count;
+      }
+    }
+
+    Vector(const Vector &other):
+      data_(nullptr),
+      size_(0),
+      capacity_(0)
+    {
+      if (other.capacity_ > 0) {
+        reallocate(other.capacity_);
+        for (std::size_t i = 0; i < other.size_; ++i) {
+          data_[i] = other.data_[i];
+        }
+        size_ = other.size_;
+      }
+    }
+
+    Vector(Vector &&other) noexcept:
+      data_(other.data_),
+      size_(other.size_),
+      capacity_(other.capacity_)
+    {
+      other.data_ = nullptr;
+      other.size_ = 0;
+      other.capacity_ = 0;
+    }
+
+    Vector &operator=(Vector other)
+    {
+      std::swap(data_, other.data_);
+      std::swap(size_, other.size_);
+      std::swap(capacity_, other.capacity_);
+      return *this;
+    }
+
+    ~Vector()
+    {
+      delete[] data_;
+    }
+
+    void pushBack(const T &value)
+    {
+      if (size_ == capacity_) {
+        reallocate(capacity_ == 0 ? 1 : capacity_ * 2);
+      }
+      data_[size_++] = value;
+    }
+
+    void pushBack(T &&value)
+    {
+      if (size_ == capacity_) {
+        reallocate(capacity_ == 0 ? 1 : capacity_ * 2);
+      }
+      data_[size_++] = std::move(value);
+    }
+
+    reference operator[](std::size_t index)
+    {
+      return data_[index];
+    }
+
+    const_reference operator[](std::size_t index) const
+    {
+      return data_[index];
+    }
+
+    std::size_t size() const noexcept
+    {
+      return size_;
+    }
+
+    std::size_t capacity() const noexcept
+    {
+      return capacity_;
+    }
+
+    bool empty() const noexcept
+    {
+      return size_ == 0;
+    }
+
+    void clear() noexcept
+    {
+      size_ = 0;
+    }
+
+    void resize(std::size_t new_size)
+    {
+      if (new_size > capacity_) {
+        reallocate(new_size);
+      }
+      if (new_size > size_) {
+        for (std::size_t i = size_; i < new_size; ++i) {
+          data_[i] = T();
+        }
+      }
+      size_ = new_size;
+    }
+
+    iterator begin() noexcept
+    {
+      return data_;
+    }
+    iterator end() noexcept
+    {
+      return data_ + size_;
+    }
+    const_iterator begin() const noexcept
+    {
+      return data_;
+    }
+    const_iterator end() const noexcept
+    {
+      return data_ + size_;
+    }
+    const_iterator cbegin() const noexcept
+    {
+      return data_;
+    }
+    const_iterator cend() const noexcept
+    {
+      return data_ + size_;
+    }
+  };
+}
+
+#endif
