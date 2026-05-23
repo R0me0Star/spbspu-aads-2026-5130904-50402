314f7d8e6dd864ee9731769d77c950d7a1c75d52
R0me0Star <eip_spb@mail.ru>
2026-05-23 15:56:28 +0300

    sha1 implementation

---
+++ pozdnyakov.egor/common/sha1.hpp
@@ -0,0 +1,38 @@
+#ifndef SHA1_HPP
+#define SHA1_HPP
+
+#include <cstddef>
+#include <string>
+#include <utility>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/sha1.hpp>
+
+namespace pozdnyakov
+{
+  namespace hash
+  {
+    struct GraphEdgeSHA1
+    {
+      std::size_t operator()(const std::pair< std::string, std::string > &p) const
+      {
+        boost::hash2::sha1_160 hasher;
+
+        std::string combined = p.first + ":" + p.second;
+
+        boost::hash2::hash_append(hasher, {}, combined);
+
+        auto digest = hasher.result();
+
+        std::size_t hash = 0;
+
+        for (std::size_t i = 0; i < digest.size(); ++i) {
+          hash ^= static_cast< std::size_t >(digest[i]) << ((i % sizeof(std::size_t)) * 8);
+        }
+
+        return hash;
+      }
+    };
+  }
+}
+
+#endif
