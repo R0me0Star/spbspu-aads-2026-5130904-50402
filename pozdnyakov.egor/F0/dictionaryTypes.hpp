d0bee9f4ad2d81404e3cc473eb9995958b68f100
R0me0Star <eip_spb@mail.ru>
2026-06-20 17:49:01 +0300

    AVL tree math

--- pozdnyakov.egor/F0/dictionaryTypes.hpp
+++ pozdnyakov.egor/F0/dictionaryTypes.hpp
@@ -65,8 +65,10 @@ namespace pozdnyakov
   {
   public:
     AvlDictionary();
+
     AvlDictionary(const AvlDictionary &other) = delete;
     AvlDictionary &operator=(const AvlDictionary &other) = delete;
+
     ~AvlDictionary();
 
     void addWord(const std::string &engWord, const std::string &rusWord, const std::string &partOfSpeech);
@@ -75,6 +77,12 @@ namespace pozdnyakov
     detail::WordNode *root_;
 
     std::size_t getHeight(const detail::WordNode *node) const;
+    int getBalanceFactor(const detail::WordNode *node) const;
+    void updateHeight(detail::WordNode *node);
+
+    detail::WordNode *rotateRight(detail::WordNode *node);
+    detail::WordNode *rotateLeft(detail::WordNode *node);
+    detail::WordNode *balance(detail::WordNode *node);
   };
 
   AvlDictionary::AvlDictionary():
@@ -95,9 +103,76 @@ namespace pozdnyakov
     }
   }
 
+  int AvlDictionary::getBalanceFactor(const detail::WordNode *node) const
+  {
+    if (node == nullptr) {
+      return 0;
+    }
+    return static_cast< int >(getHeight(node->right_)) - static_cast< int >(getHeight(node->left_));
+  }
+
+  void AvlDictionary::updateHeight(detail::WordNode *node)
+  {
+    if (node != nullptr) {
+      std::size_t leftHeight = getHeight(node->left_);
+      std::size_t rightHeight = getHeight(node->right_);
+      node->height_ = 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+    }
+  }
+
+  detail::WordNode *AvlDictionary::rotateRight(detail::WordNode *node)
+  {
+    detail::WordNode *leftChild = node->left_;
+    detail::WordNode *rightOfLeftChild = leftChild->right_;
+
+    leftChild->right_ = node;
+    node->left_ = rightOfLeftChild;
+
+    updateHeight(node);
+    updateHeight(leftChild);
+
+    return leftChild;
+  }
+
+  detail::WordNode *AvlDictionary::rotateLeft(detail::WordNode *node)
+  {
+    detail::WordNode *rightChild = node->right_;
+    detail::WordNode *leftOfRightChild = rightChild->left_;
+
+    rightChild->left_ = node;
+    node->right_ = leftOfRightChild;
+
+    updateHeight(node);
+    updateHeight(rightChild);
+
+    return rightChild;
+  }
+
+  detail::WordNode *AvlDictionary::balance(detail::WordNode *node)
+  {
+    updateHeight(node);
+    int balanceFactor = getBalanceFactor(node);
+
+    if (balanceFactor > 1) {
+      if (getBalanceFactor(node->right_) < 0) {
+        node->right_ = rotateRight(node->right_);
+      }
+      return rotateLeft(node);
+    }
+
+    if (balanceFactor < -1) {
+      if (getBalanceFactor(node->left_) > 0) {
+        node->left_ = rotateLeft(node->left_);
+      }
+      return rotateRight(node);
+    }
+
+    return node;
+  }
+
   void AvlDictionary::addWord(const std::string &engWord, const std::string &rusWord, const std::string &partOfSpeech)
   {
-    std::cout << "Added word: " << engWord << " - " << rusWord << "\n";
+    std::cout << "Method prepared for: " << engWord << "\n";
   }
 
 }
