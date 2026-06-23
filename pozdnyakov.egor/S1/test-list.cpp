8b530bf9cc039b4ae1669c61cda67a8dea1aba45
R0me0Star <eip_spb@mail.ru>
2026-06-23 20:21:40 +0300

    updated list & tests

--- pozdnyakov.egor/S1/test-list.cpp
+++ pozdnyakov.egor/S1/test-list.cpp
@@ -14,9 +14,10 @@ BOOST_AUTO_TEST_CASE(testListInsertAndIterators)
   BOOST_CHECK_EQUAL(list.front(), 10);
 
   list.insertAfter(list.begin(), 20);
+  list.pushBack(30);
 
-  const int expected[] = {10, 20};
-  int i = 0;
+  const int expected[] = {10, 20, 30};
+  size_t i = 0;
   for (auto it = list.cbegin(); it != list.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected[i]);
     ++i;
@@ -43,18 +44,18 @@ BOOST_AUTO_TEST_CASE(testListCopyExceptionSafety)
 BOOST_AUTO_TEST_CASE(testListSplice)
 {
   List< int > list1;
-  list1.pushFront(2);
-  list1.pushFront(1);
+  list1.pushBack(1);
+  list1.pushBack(2);
 
   List< int > list2;
-  list2.pushFront(4);
-  list2.pushFront(3);
+  list2.pushBack(3);
+  list2.pushBack(4);
 
   list1.splice(list1.cend(), list2);
 
   BOOST_CHECK(list2.empty());
   const int expected[] = {1, 2, 3, 4};
-  int i = 0;
+  size_t i = 0;
   for (auto it = list1.cbegin(); it != list1.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected[i++]);
   }
@@ -63,23 +64,23 @@ BOOST_AUTO_TEST_CASE(testListSplice)
 BOOST_AUTO_TEST_CASE(testListSpliceSingle)
 {
   List< int > list1;
-  list1.pushFront(2);
-  list1.pushFront(1);
+  list1.pushBack(1);
+  list1.pushBack(2);
 
   List< int > list2;
-  list2.pushFront(4);
-  list2.pushFront(3);
+  list2.pushBack(3);
+  list2.pushBack(4);
 
   list1.splice(list1.begin(), list2, list2.begin());
 
   const int expected1[] = {3, 1, 2};
-  int i = 0;
+  size_t i = 0;
   for (auto it = list1.cbegin(); it != list1.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected1[i++]);
   }
 
   const int expected2[] = {4};
-  int j = 0;
+  size_t j = 0;
   for (auto it = list2.cbegin(); it != list2.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected2[j++]);
   }
@@ -88,18 +89,20 @@ BOOST_AUTO_TEST_CASE(testListSpliceSingle)
 BOOST_AUTO_TEST_CASE(testListMerge)
 {
   List< int > list1;
-  list1.pushFront(3);
-  list1.pushFront(1);
+  list1.pushBack(1);
+  list1.pushBack(3);
+  list1.pushBack(5);
 
   List< int > list2;
-  list2.pushFront(4);
-  list2.pushFront(2);
+  list2.pushBack(2);
+  list2.pushBack(4);
+  list2.pushBack(6);
 
   list1.merge(list2);
 
   BOOST_CHECK(list2.empty());
-  const int expected[] = {1, 2, 3, 4};
-  int i = 0;
+  const int expected[] = {1, 2, 3, 4, 5, 6};
+  size_t i = 0;
   for (auto it = list1.cbegin(); it != list1.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected[i++]);
   }
@@ -108,15 +111,16 @@ BOOST_AUTO_TEST_CASE(testListMerge)
 BOOST_AUTO_TEST_CASE(testListSort)
 {
   List< int > list;
-  list.pushFront(1);
-  list.pushFront(4);
-  list.pushFront(2);
-  list.pushFront(3);
+  list.pushBack(4);
+  list.pushBack(2);
+  list.pushBack(5);
+  list.pushBack(1);
+  list.pushBack(3);
 
   list.sort();
 
-  const int expected[] = {1, 2, 3, 4};
-  int i = 0;
+  const int expected[] = {1, 2, 3, 4, 5};
+  size_t i = 0;
   for (auto it = list.cbegin(); it != list.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected[i++]);
   }
@@ -125,17 +129,18 @@ BOOST_AUTO_TEST_CASE(testListSort)
 BOOST_AUTO_TEST_CASE(testListPartition)
 {
   List< int > list;
-  list.pushFront(4);
-  list.pushFront(3);
-  list.pushFront(2);
-  list.pushFront(1);
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(3);
+  list.pushBack(4);
+  list.pushBack(5);
 
   list.partition([](const int &val) {
-    return val % 2 != 0;
+    return val % 2 == 0;
   });
 
-  const int expected[] = {1, 3, 2, 4};
-  int i = 0;
+  const int expected[] = {2, 4, 1, 3, 5};
+  size_t i = 0;
   for (auto it = list.cbegin(); it != list.cend(); ++it) {
     BOOST_CHECK_EQUAL(*it, expected[i++]);
   }
