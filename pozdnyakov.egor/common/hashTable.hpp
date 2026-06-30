0a83667960d064814e660893e87cef440dac9f20
R0me0Star <eip_spb@mail.ru>
2026-06-30 15:47:20 +0300

    added methods loadFactor & maxBucketSize; array bucketSize_; added limits

--- pozdnyakov.egor/common/hashTable.hpp
+++ pozdnyakov.egor/common/hashTable.hpp
@@ -1,6 +1,7 @@
 #ifndef HASH_TABLE_HPP
 #define HASH_TABLE_HPP
 
+#include <functional>
 #include <stdexcept>
 #include <utility>
 #include "list.hpp"
@@ -17,14 +18,27 @@ namespace pozdnyakov
     }
   };
 
+  struct DefaultSlotUpdater
+  {
+    std::size_t operator()(std::size_t before) const
+    {
+      return (before < 10) ? 20 : before * 2;
+    }
+  };
+
   template< class Key, class Value, class Hash, class Equal = DefaultEqual< Key > >
   class HashTable
   {
   private:
     Vector< List< std::pair< Key, Value > > > buckets_;
+    Vector< std::size_t > bucketSizes_;
     std::size_t size_;
     Hash hashObj_;
     Equal equalObj_;
+
+    float maxLoadFactor_;
+    std::size_t maxBucketSizeLimit_;
+    std::function< std::size_t(std::size_t) > slotUpdater_;
 
   public:
     class iterator
@@ -113,13 +127,60 @@ namespace pozdnyakov
     };
 
     HashTable():
-      size_(0)
+      size_(0),
+      maxLoadFactor_(1.0f),
+      maxBucketSizeLimit_(10),
+      slotUpdater_(DefaultSlotUpdater())
     {}
+
+    float loadFactor() const
+    {
+      if (buckets_.size() == 0) {
+        return 0.0f;
+      }
+      return static_cast< float >(size_) / static_cast< float >(buckets_.size());
+    }
+
+    std::size_t maxBucketSize() const
+    {
+      std::size_t m = 0;
+      for (std::size_t i = 0; i < bucketSizes_.size(); ++i) {
+        if (bucketSizes_[i] > m) {
+          m = bucketSizes_[i];
+        }
+      }
+      return m;
+    }
+
+    void maxLoadFactor(float mlf)
+    {
+      maxLoadFactor_ = mlf;
+    }
+
+    float maxLoadFactor() const
+    {
+      return maxLoadFactor_;
+    }
+
+    void maxBucketSizeLimit(std::size_t mbs)
+    {
+      maxBucketSizeLimit_ = mbs;
+    }
+
+    std::size_t maxBucketSizeLimit() const
+    {
+      return maxBucketSizeLimit_;
+    }
+
+    void setSlotUpdater(std::function< std::size_t(std::size_t) > updater)
+    {
+      slotUpdater_ = updater;
+    }
 
     void add(Key k, Value v)
     {
       if (buckets_.size() == 0) {
-        rehash(16);
+        rehash(slotUpdater_(0));
       }
 
       std::size_t idx = hashObj_(k) % buckets_.size();
@@ -132,7 +193,12 @@ namespace pozdnyakov
       }
 
       buckets_[idx].pushFront(std::make_pair(std::move(k), std::move(v)));
+      ++bucketSizes_[idx];
       ++size_;
+
+      if (loadFactor() > maxLoadFactor_ || bucketSizes_[idx] > maxBucketSizeLimit_) {
+        rehash(slotUpdater_(buckets_.size()));
+      }
     }
 
     bool drop(const Key &k)
@@ -156,6 +222,7 @@ namespace pozdnyakov
 
       if (found) {
         buckets_[idx] = std::move(newList);
+        --bucketSizes_[idx];
         --size_;
       }
 
@@ -197,15 +264,19 @@ namespace pozdnyakov
       }
       Vector< List< std::pair< Key, Value > > > newBuckets;
       newBuckets.resize(slots);
+      Vector< std::size_t > newBucketSizes;
+      newBucketSizes.resize(slots);
 
       for (std::size_t i = 0; i < buckets_.size(); ++i) {
         for (auto it = buckets_[i].begin(); it != buckets_[i].end(); ++it) {
           std::size_t idx = hashObj_((*it).first) % slots;
           newBuckets[idx].pushFront(std::move(*it));
+          ++newBucketSizes[idx];
         }
       }
 
       buckets_ = std::move(newBuckets);
+      bucketSizes_ = std::move(newBucketSizes);
     }
 
     std::size_t size() const
